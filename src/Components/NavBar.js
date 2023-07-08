5d03cb3991395aea3d713c224c00851a5a3acc73
Baburaj <sbrbabu123@gmail.com>
2023-07-08 09:27:28 +0530

    Added Cards

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+
+const NavBar = () => (
+    <div className="navbar">
+        <div className="nav-header">
+            <img src="src\Components\Images\Blogging_app_icon.png" alt="BlogApp"></img>
+            <h1>Blog App</h1>
+        </div>
+    </div>
+)
+
+export NavBar;
